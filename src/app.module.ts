a054197f3f47a7b12347ddcd4f9fd47097fdcb86
Shahzada Shoaib <124442653+Shoaib-bit@users.noreply.github.com>
2025-10-03 14:42:28 +0500

    env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
+import { CommonModule } from './common'
 
 @Module({
-    imports: [],
+    imports: [CommonModule],
     controllers: [],
     providers: []
 })
