94932d998625b991d18ce577ee46ef58c181a396
Shahzada Shoaib <124442653+Shoaib-bit@users.noreply.github.com>
2025-10-03 20:34:20 +0500

    complete signup api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,9 +6,12 @@ import {
     Post
 } from '@nestjs/common'
 import { LoginDto, SignupDto } from './auth.dto'
+import { AuthService } from './auth.service'
 
 @Controller()
 export class AuthController {
+    constructor(private authService: AuthService) {}
+
     @Post('login')
     login(@Body() loginDto: LoginDto) {
         try {
@@ -25,10 +28,21 @@ export class AuthController {
     }
 
     @Post('signup')
-    signup(@Body() signupDto: SignupDto) {
+    async signup(@Body() signupDto: SignupDto) {
         try {
-            //Todo will implement
-            console.log(signupDto)
+            if (signupDto.password !== signupDto.confirmPassword) {
+                throw new Error('Password not match with confirm Password')
+            }
+            const user = await this.authService.saveUser(signupDto)
+
+            return {
+                message: 'Signup Successfully',
+                success: true,
+                data: {
+                    username: user.username,
+                    email: user.email
+                }
+            }
         } catch (error) {
             if (error.message) {
                 throw new BadRequestException(error.message)
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common'
+import { DatabaseService } from 'src/common/services'
+import { PasswordUtil } from 'src/common/utils'
+import { SignupDto } from './auth.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly databaseService: DatabaseService) {}
+
+    async saveUser(user: SignupDto) {
+        try {
+            const emailExist = await this.databaseService.user.findUnique({
+                where: { email: user.email }
+            })
+
+            if (emailExist) {
+                throw new Error('User with this email already exists')
+            }
+
+            const hashedPassword = await PasswordUtil.hashPassword(
+                user.password
+            )
+
+            const newUser = await this.databaseService.user.create({
+                data: {
+                    username: user.username,
+                    email: user.email,
+                    password: hashedPassword
+                }
+            })
+
+            return newUser
+        } catch (error) {
+            if (error.message.includes('email already exists')) {
+                throw error
+            }
+            throw new Error(`Failed to save user: ${error.message}`)
+        }
+    }
+}
